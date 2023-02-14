17966376ce364e3d5c93d8b5435ed6a6bf626f0d
Miluche <meshkipla@gmail.com>
2023-02-14 12:01:15 +0300

    added a constructor and multiple variable initializations

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,36 @@
 class Dog
-  @name = "nil"
-  def initialize(name = nil)
-    @name = name || self.name
+  def initialize(args = {})
+    @name = args["name"] || "nill"
+    @age = args["age"] || 8
   end
   def name=(name)
     @name = name
   end
+
   def name
     @name
+  end
+
+  def age=(age)
+    @age = age
+  end
+  def age
+    @age
+  end
+
+  def to_s
+    "Name: #{@name}, Age: #{@age}"
   end
 end
 
 lassie = Dog.new
 lassie.name = "Lassie"
-puts lassie.name
+puts lassie.to_s
 
-odie = Dog.new("Odie")
-puts odie.name
+odie = Dog.new({"name" => "Odie"})
+odie.age = 10
+puts odie.to_s
+
+erica = Dog.new({"name" => "Erica", "age"=>8})
+puts erica.to_s
+

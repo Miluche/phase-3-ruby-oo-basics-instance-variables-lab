bb8792cd4f8aae7929035e7123474ac4c4f29250
Miluche <meshkipla@gmail.com>
2023-02-14 11:21:48 +0300

    tested instance variables in Dog class

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
+  @name = "nil"
+  def initialize(name = nil)
+    @name = name || self.name
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
+
+odie = Dog.new("Odie")
+puts odie.name
